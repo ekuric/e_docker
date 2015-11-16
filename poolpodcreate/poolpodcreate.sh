2b9beb012bce61a0d23a2b52672afc5cecae91f0
Elvir Kuric <elvirkuric@gmail.com>
2015-11-16 14:02:18 +0100

    updated poolpodcreate.sh to support 'delete' option
    Signed-off-by: Elvir Kuric <elvirkuric@gmail.com>

--- poolpodcreate/poolpodcreate.sh
+++ poolpodcreate/poolpodcreate.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
-IMAGE="$1"
-RANGE="$2"
+ACTION="$1"
+IMAGE="$2"
+RANGE="$3"
 COUNTER=0
 # script to create number of pods
 
 usage() {
-    printf "./poolpodcreate <IMAGE> <RANGE> \n"
+    printf "./poolpodcreate <ACTION> [<IMAGE>] [<RANGE>] \n"
     printf "where is:\n"
     printf "<IMAGE> docker image which will be used for pods \n"
     printf "<RANGE> number of pods we want to create \n"
+    printf "Example to create pods: ./poolcreate.sh create image_ssh 100\n"
+    printf "Example to delete pods: ./poolcreate.sh delete \n"
 }
 
 
 
-if [ ["$EUID" -ne 0 ] || [ "$#" -ne 2 ] ; then
+if  [ "$EUID" -ne 0 ]; then
     printf "You have to be root to run script and script takes below parameters\n"
     usage
     exit 0
 fi
-while [ $COUNTER -lt $RANGE ] ;do
+
+create_pods() {
+    while [ $COUNTER -lt $RANGE ] ;do
 
 cat <<EOF > pod.yml
 apiVersion: v1
@@ -48,6 +53,25 @@ EOF
 	COUNTER=$[$COUNTER+1]
 done
 
+}
+
+delete_pods() {
+    for pod in $(oc get pods | grep pod- | awk '{ print $1 }') ; do
+        oc delete pod $pod
+    done
+}
+
+case "$ACTION" in
+    c|create)
+        create_pods ;;
+    d|delete)
+        delete_pods ;;
+    *)
+        printf "Wrong option ... check again\n"; usage ;;
+esac
+
 printf "\n"
 printf "$RANGE pods were created...\n"
 printf " --------------------------- \n"
+
+
